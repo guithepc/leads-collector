80089f65951f75209b47827390d881295eea47b6
guithepc <guilhermepc@yahoo.com>
2025-02-16 23:46:46 -0300

    impl

--- src/main/java/com/pcaut/leads_collector/controller/Controller.java
+++ src/main/java/com/pcaut/leads_collector/controller/Controller.java
@@ -1,24 +1,71 @@
 package com.pcaut.leads_collector.controller;
 
 import com.pcaut.leads_collector.services.impl.ConnectionServiceImpl;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-import java.io.IOException;
+import java.io.*;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 @RestController
 @RequestMapping("/services")
 public class Controller {
 
-    @GetMapping(value="/connect")
+    @PostMapping(value="/connect")
     public ResponseEntity<List<String>> conncect() throws InterruptedException, IOException {
         ConnectionServiceImpl connectionService = new ConnectionServiceImpl();
 
         List<String> lista = connectionService.connect();
         return ResponseEntity.ok(lista);
+    }
+
+    @GetMapping(value="/hh")
+    public void hh() throws InterruptedException, IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int arrCount = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<Integer> arr = IntStream.range(0, arrCount).mapToObj(i -> {
+                    try {
+                        return bufferedReader.readLine().replaceAll("\\s+$", "");
+                    } catch (IOException ex) {
+                        throw new RuntimeException(ex);
+                    }
+                })
+                .map(String::trim)
+                .map(Integer::parseInt)
+                .collect(Collectors.toList());
+
+        int k = Integer.parseInt(bufferedReader.readLine().trim());
+
+        String result = Controller.findNumber(arr, k);
+
+        bufferedWriter.write(result);
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+
+    }
+
+    public static String findNumber(List<Integer> arr, int k) {
+        String result = new String();
+        for(int i =0; i < arr.size(); i++){
+            Integer number = arr.get(i);
+
+            if (number == k) {
+
+                result = "YES";
+            } else {
+                result = "NO";
+            }
+        }
+        System.out.println(result);
+        return result;
+
+
     }
 }
--- src/main/java/com/pcaut/leads_collector/services/impl/ConnectionServiceImpl.java
+++ src/main/java/com/pcaut/leads_collector/services/impl/ConnectionServiceImpl.java
@@ -2,17 +2,12 @@ package com.pcaut.leads_collector.services.impl;
 
 import com.pcaut.leads_collector.services.ConnectionService;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.FluentWait;
-import org.openqa.selenium.support.ui.Wait;
 import org.openqa.selenium.support.ui.WebDriverWait;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 
-import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
@@ -24,54 +19,53 @@ import java.util.List;
 @Configuration
 @Slf4j
 public class ConnectionServiceImpl implements ConnectionService {
-    @Value("${EMAIL}")
-    String email;
     @Override
     public List<String> connect() throws InterruptedException, IOException {
         WebDriver chrome = new ChromeDriver();
         chrome.get("https://app.herospark.com/login");
 
-
-        Thread.sleep(15000);
-        WebElement loginButton = chrome.findElement(By.xpath("//*[@id=\"app\"]/section/div/div/form/button"));
-        loginButton.click();
-
         WebDriverWait wait = new WebDriverWait(chrome, Duration.ofSeconds(20));
 
-        Thread.sleep(2000);
-        WebElement hamb = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@id=\"app\"]/div/header/div/div/button")));
-        hamb.click();
+        Thread.sleep(15000);
+        WebElement loginButton2 = chrome.findElement(By.xpath("//*[@id=\"app\"]/section/div/div/form/button"));
+        loginButton2.click();
+
 
         Thread.sleep(2000);
-        WebElement funnels = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@id=\"app\"]/div/main/aside/div/div[2]/ul/li[5]")));
+        WebElement hamburguer = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@id=\"app\"]/div/header/div/div/button")));
+        hamburguer.click();
+
+        Thread.sleep(2000);
+        WebElement funnels = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@id=\"app\"]/div/main/aside/div/div[2]/ul/li[4]/a/div/i")));
         funnels.click();
 
+        WebElement funilEscolhido = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@id=\"funnel-list-section\"]/div/div/div/div/div[1]/div[1]/div[2]/a")));
+        funilEscolhido.click();
+
         return this.collectList(wait);
-
-
-
     }
 
     public List<String> collectList(WebDriverWait wait){
         int j = 9;
         List<String> finalList = new ArrayList<>();
         try {
-            for (int i = 0; i < 299; i++){
+            for (int i = 0; i < 1300; i++){
                 List<String> list = this.loop(j, finalList, wait);
                 WebElement buttonNext = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@id=\"app\"]/div/main/div/div/section[2]/div/div[4]/div[2]/ul/li[7]/button/i")));
                 buttonNext.click();
                 finalList.addAll(list);
                 System.out.println(i);
+                Thread.sleep(1000);
             }
 
         }catch (TimeoutException e){
             log.error(e.getMessage());
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
         } finally {
             this.saveToFile(finalList, "C:\\Users\\pctheone\\Documents\\Repositories\\leads-collector\\leads.txt");
         }
         return finalList;
-
-
     }
 
     public List<String> loop(int j, List<String> list, WebDriverWait wait){
